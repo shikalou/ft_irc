ee1825af6ce17b3c9bb7acc1c3d43dca2864b2de
karaskp <m.couppe@hotmail.fr>
2023-06-14 19:07:28 +0200

    [marine]attempted to do something with the epoll_wait loop but failed

---
+++ last_v/Server.hpp
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Server.hpp                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ldinaut <ldinaut@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/05/29 13:25:21 by ldinaut           #+#    #+#             */
+/*   Updated: 2023/06/14 16:46:16 by ldinaut          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <sys/socket.h>
+#include <sys/epoll.h>
+#include <netinet/in.h>
+#include <cstdint>
+#include <cstddef>
+#include <iostream>
+#include <unistd.h>
+#include <fcntl.h>
+
+class Server
+{
+	public:
+		Server(int port, std::string pass);
+		~Server();
+
+		std::string	password;
+		int			port;
+		int			epoll_fd;
+};
--- last_v/main.cpp
+++ last_v/main.cpp
@@ -6,7 +6,7 @@
 /*   By: ldinaut <ldinaut@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/26 17:14:07 by ldinaut           #+#    #+#             */
-/*   Updated: 2023/06/14 16:49:00 by ldinaut          ###   ########.fr       */
+/*   Updated: 2023/06/14 18:54:52 by mcouppe          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,8 +25,6 @@ int main(int argc, char *argv[])
 	toto.epoll_fd = epoll_create1(0);
 
 	int sock = socket(AF_INET, SOCK_STREAM, 0);
-	int flags = fcntl(sock, F_GETFL, 0);
-	fcntl(sock, F_SETFL, flags | O_NONBLOCK);
 	if (sock == -1)
 	{
 		std::cout << "socket failed" << std::endl;
@@ -48,14 +46,20 @@ int main(int argc, char *argv[])
 		exit(1);
 	}
 	int addrlen = sizeof(sockaddr);
+	std::cout << "sock before accept :" << sock << std::endl;
 	int fd_co = accept(sock, (struct sockaddr*)&sockaddr, (socklen_t*)&addrlen);
+	if (fd_co == -1){
+		std::cerr << "accept issue : " << errno << std::endl;
+		return (1);
+	}
+	std::cout << "sock after accept : " << sock << "fd_client = " << fd_co << std::endl;
 	ev->data.fd = sock;
 	ev->events = EPOLLIN;
 	epoll_ctl(toto.epoll_fd, EPOLL_CTL_ADD, sock, ev);
-	int j = 0;
 	while (1)
 	{
 		int event = epoll_wait(toto.epoll_fd, ev, 5, 10000);
+		std::cout << "event after epoll_wait : " << event << std::endl;
 		if (event < 0)
 		{
 			std::cout << "event a eu un souci : " << errno << std::endl;
@@ -63,16 +67,50 @@ int main(int argc, char *argv[])
 		}
 		else if (event > 0)
 		{
-			std::cout << "event = " << event << "    lol = " << ev->data.fd << std::endl;
+/*
+
+		ici je fais exactement pareil que dans le man de epoll (cf https://manpages.ubuntu.com/manpages/focal/fr/man7/epoll.7.html)
+		y'a juste une partie ou dans le man ils appellent une fonction pour mettre socket non blocking
+		du coup j'ai remis les fcntl qui faisaient tout buguer en haut mais ca marche pas ni avec ni sans lol
+
+		aussi, seconde diff avec le man : la structure ev il aime pas trop dc peut etre un pblm avec ca ?
+
+*/
+
+			for (int n = 0; n < event; ++n){
+				if (ev[n].data.fd == sock){
+					addrlen = sizeof(sockaddr);
+					fd_co = accept(sock, (struct sockaddr*)&sockaddr, (socklen_t*)&addrlen);
+					if (fd_co == -1){
+						std::cerr << "ze kroi ke zé vu un grominet" << std::endl;
+						return (1);
+					}
+		//			int flags = fcntl(fd_co, F_GETFL, 0);
+		//			fcntl(fd_co, F_SETFL, flags | O_NONBLOCK);
+					ev->events = EPOLLIN | EPOLLET;
+					ev->data.fd = fd_co;
+					if (epoll_ctl(sock, EPOLL_CTL_ADD, fd_co, ev) == -1){
+						std::cerr << "error on epoll_ctl : fd_co" << std::endl;
+						return (1);
+					}
+				}
+				else {
+					std::cout << "ici on doit utiliser fd ???" << std::endl;
+					break;
+				}
+			}
+/*			std::cout << "event = " << event << "    lol = " << ev->data.fd << std::endl;
 			if (event == ev->data.fd)
 				std::cout << "NOUVEAU CLIENT" << std::endl;
 			else
 			{
 				std::cout << "COMMAND : " << std::endl;
 				char toto1[4608];
-				int i = read(fd_co, toto1, 4608);
+//				int i = read(fd_co, toto1, 4608);
+				read(fd_co, toto1, 4608);
 				std::cout << "client : " << toto1 << std::endl;
-			}
+				break;
+			}*/
 			// if (j == 0)
 			// {
 			// 	std::string str = "CAP * LS :";
