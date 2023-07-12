638956896489086a40ca0392a14f49dc6c7a3bdb
jtaravel <jtaravel@student.42.fr>
2023-07-12 20:02:31 +0200

    faut fix part

--- srcs/cmd_files/kick.cpp
+++ srcs/cmd_files/kick.cpp
@@ -6,7 +6,7 @@
 /*   By: ldinaut <ldinaut@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/06 15:36:31 by ldinaut           #+#    #+#             */
-/*   Updated: 2023/07/12 13:08:40 by ldinaut          ###   ########.fr       */
+/*   Updated: 2023/07/12 19:42:44 by jtaravel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -48,45 +48,45 @@ std::vector<std::string>	Commands::kick_cmd(Client *client)
 	std::string res = "You've been kicked from channel " + _cmd_args[0] + "\r\n";
 	send(client_tmp->_sock, res.c_str(), res.length(), 0);
 	return (reponse);
-	// std::vector<Client *>::iterator it = chan_tmp->_clients.begin();
-	// for (; it != chan_tmp->_clients.end(); ++it)
-	// {
-	// 	if ((*it)->getNick() == _cmd_args[1])
-	// 	{
-	// 		chan_tmp->_clients.erase(it);
-	// 		break;
-	// 	}
-	// }
-	// std::vector<Channel *>::iterator ite = client_tmp->_chans.begin();
-	// for (; ite != client_tmp->_chans.end(); ++it)
-	// {
-	// 	if ((*ite)->getTitle() == _cmd_args[0])
-	// 	{
-	// 		client_tmp->_chans.erase(ite);
-	// 		break ;
-	// 	}
-	// }
-	// ite = server._channels.begin();
-	// for (;ite != server._channels.end(); ++ite)
-	// {
-	// 	if ((*ite)->getTitle() == _cmd_args[0])
-	// 	{
-	// 		(*ite)->_clients.erase(it);
-	// 		std::string res = "You've been kicked from channel " + _cmd_args[0] + "\r\n";
-	// 		send(client_tmp->_sock, res.c_str(), res.length(), 0);
-	// 		break ;
-	// 	}
-	// }
-	// std::string ret = ":" + client->getNick() + " KICK " + _cmd_args[0] + " " +  _cmd_args[1];
-	// if (_cmd_args.size() >= 3)
-	// {
-	// 	for (size_t i = 0; i < _cmd_args.size(); ++i)
-	// 	{
-	// 		ret += " ";
-	// 		ret += _cmd_args[i];
-	// 	}
-	// }
-	// ret += "\r\n";
-	// reponse.push_back(ret);
-	// return (reponse);
+	/*std::vector<Client *>::iterator it = chan_tmp->_clients.begin();
+	for (; it != chan_tmp->_clients.end(); ++it)
+	{
+		if ((*it)->getNick() == _cmd_args[1])
+		{
+			chan_tmp->_clients.erase(it);
+			break;
+		}
+	}
+	std::vector<Channel *>::iterator ite = client_tmp->_chans.begin();
+	for (; ite != client_tmp->_chans.end(); ++it)
+	{
+		if ((*ite)->getTitle() == _cmd_args[0])
+		{
+			client_tmp->_chans.erase(ite);
+			break ;
+		}
+	}
+	ite = server._channels.begin();
+	for (;ite != server._channels.end(); ++ite)
+	{
+		if ((*ite)->getTitle() == _cmd_args[0])
+		{
+			(*ite)->_clients.erase(it);
+			std::string res = "You've been kicked from channel " + _cmd_args[0] + "\r\n";
+			send(client_tmp->_sock, res.c_str(), res.length(), 0);
+			break ;
+		}
+	}
+	std::string ret = ":" + client->getNick() + " KICK " + _cmd_args[0] + " " +  _cmd_args[1];
+	if (_cmd_args.size() >= 3)
+	{
+		for (size_t i = 0; i < _cmd_args.size(); ++i)
+		{
+			ret += " ";
+			ret += _cmd_args[i];
+		}
+	}
+	ret += "\r\n";
+	reponse.push_back(ret);
+	return (reponse);*/
 }
--- srcs/cmd_files/quit.cpp
+++ srcs/cmd_files/quit.cpp
@@ -6,7 +6,7 @@
 /*   By: ldinaut <ldinaut@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/06 12:35:07 by ldinaut           #+#    #+#             */
-/*   Updated: 2023/07/12 18:44:58 by ldinaut          ###   ########.fr       */
+/*   Updated: 2023/07/12 19:59:36 by jtaravel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,23 +20,9 @@ std::vector<std::string>	Commands::quit(Client *client)
 	_cmd_args.clear();
 	std::size_t	size = client->_chans.size();
 	std::size_t	i = 0;
-	std::vector<Channel *> lulu;
 	for (std::vector<Channel *>::iterator it = client->_chans.begin(); it != client->_chans.end(); ++it)
 	{
-		lulu.push_back(*it);
-		std::cout << "BLABLABLABLA\n\n\n" << (*it)->getTitle() << "\n\n\n\n";
-	}
-	for (std::vector<Channel *>::iterator it = lulu.begin(); it != lulu.end(); ++it)
-	{
-		std::cout << "BLABLABLABLA\n\n\n" << (*it)->getTitle() << std::endl;
 		_cmd_args[0] = (*it)->getTitle();
-	// pour resoudre double message qd part + quit
-	// possible de mettre un bool dans part
-	// si true --> on est dans quit --> on esquive les rpl
-	// else on fait tt comme d'hab
-	// potentiellement on peut regler pblm de part pas total
-	// (qd juste part ou kick, on recoit encore les rpl de topic)
-	// qui pour etre regle fait des enormes invalid read
 		part(client);
 		sender(reponse, "");
 		reponse.clear();
@@ -59,6 +45,5 @@ std::vector<std::string>	Commands::quit(Client *client)
 	ret += "\r\n";
 	reponse.push_back(ret);
 	server._clients.erase(client->_sock);
-	lulu.clear();
 	return (reponse);
 }
